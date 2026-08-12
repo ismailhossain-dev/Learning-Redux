346db775c25b8aaaa2d95fec7146eaff26b4f7b3
sabbir <programmarsabbir@gmail.com>
2026-08-12 22:54:39 +0600

    configure-redux-slice-done

--- fundametails-of-redux-1/src/redux/counter/counterSlice.ts
+++ fundametails-of-redux-1/src/redux/counter/counterSlice.ts
@@ -1,1 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 //========we are write all state here in redux=======
+
+//========type define ts=========
+
+interface initialState {
+    value: number
+}
+
+//slice create korar somoy name , initialState, reducers must be provide korte hobe
+const initialState:initialState = {
+    value: 0, 
+}
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState,//etar value ta ekane declear kora jai but ekto big hower karone bahire declear kora hoiche
+
+    
+    reducers: {
+        //create a increment function 
+        increment: (state) => {
+            state.value = state.value +1
+        },
+
+        //create a decrement function
+        decrement: (state) => {
+            state.value  = state.value -1 
+        }
+
+    }
+})
+
+export const {increment, decrement}= counterSlice.actions;
+
+export default counterSlice.reducer; 
--- fundametails-of-redux-1/src/redux/store.ts
+++ fundametails-of-redux-1/src/redux/store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+//ekane amra counterSlice take connect korbo 
+import counterReducer from  "./counter/counterSlice"
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    //============connect counterSlice with store.ts=======
+    counter : counterReducer
+  },
 })
 
+
+//
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
