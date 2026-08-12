473f96d7e3cde4adb48225ca33e05487b9e300db
sabbir <programmarsabbir@gmail.com>
2026-08-12 20:35:58 +0600

    install vite+tailwind+shadcn

---
+++ fundametails-of-redux-1/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <div className="text-4xl text-red-500 font-bold1">React+tailwindCss+Shadcn+Redux</div>
+  </div>
+  )
+}
+
+export default App
